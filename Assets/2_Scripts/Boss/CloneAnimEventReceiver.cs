d2cd94dd4db927785b27972f7a586dd019d82b08
최솔 <vahyeon2510@sonline20.sen.go.kr>
2025-09-25 11:53:03 +0900

    오류수정

--- Assets/2_Scripts/Boss/CloneAnimEventReceiver.cs
+++ Assets/2_Scripts/Boss/CloneAnimEventReceiver.cs
@@ -1,13 +1,13 @@
-using UnityEngine;
+////using UnityEngine;
 
-[DisallowMultipleComponent]
-public class CloneAnimEventReceiver : MonoBehaviour
-{
-    [Tooltip("루트에 있는 JangsanbeomClone(또는 분신 스크립트)")]
-    public JangsanbeomClone owner;
+//[DisallowMultipleComponent]
+//public class CloneAnimEventReceiver : MonoBehaviour
+//{
+//    [Tooltip("루트에 있는 JangsanbeomClone(또는 분신 스크립트)")]
+//    public JangsanbeomClone owner;
 
-    // 애니메이션 이벤트에서 그대로 호출되는 메서드들
-    // 애니메이션 클립의 이벤트 이름은 이 두 개를 그대로 사용하세요.
-    public void OnClawHitFrame() { owner?.OnClawHitFrame(); }
-    public void OnClawFakeHitFrame() { owner?.OnClawFakeHitFrame(); }
-}
+//    // 애니메이션 이벤트에서 그대로 호출되는 메서드들
+//    // 애니메이션 클립의 이벤트 이름은 이 두 개를 그대로 사용하세요.
+//    //public void OnClawHitFrame() { owner?.OnClawHitFrame(); }
+//    //public void OnClawFakeHitFrame() { owner?.OnClawFakeHitFrame(); }
+//}
